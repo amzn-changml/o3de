6e883b7b939c1c50117f7e1074cf54375295acc7
Mike Chang <changml@amazon.com>
2024-08-02 21:54:49 -0700

    Update Configurations_msvc.cmake
    Signed-off-by: Mike Chang <changml@amazon.com>

--- cmake/Platform/Common/MSVC/Configurations_msvc.cmake
+++ cmake/Platform/Common/MSVC/Configurations_msvc.cmake
@@ -194,7 +194,7 @@ if(ccache_exe)
     # By default Visual Studio generators will use /Zi which is not compatible
     # with ccache, so tell Visual Studio to use /Z7 instead.
     message(STATUS "Setting MSVC debug information format to 'Embedded'")
-    set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT "$<$<CONFIG:Profile,Release,Debug>:Embedded>")
+    set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT "$<$<CONFIG:Debug,RelWithDebInfo,Profile,Release>:Embedded>")
     ly_append_configurations_options(
         COMPILATION_PROFILE
             /Z7

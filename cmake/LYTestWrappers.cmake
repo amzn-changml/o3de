7791a8390889b78097e0c4b840ddd23c0c0b997c
Mike Chang <changml@amazon.com>
2024-10-10 23:23:50 -0700

    Increase LY_TEST_DEFAULT_TIMEOUT to 2500s
    Signed-off-by: Mike Chang <changml@amazon.com>

--- cmake/LYTestWrappers.cmake
+++ cmake/LYTestWrappers.cmake
@@ -22,7 +22,7 @@ ly_set(LY_TEST_GLOBAL_KNOWN_SUITE_NAMES "smoke" "main" "periodic" "benchmark" "s
 ly_set(LY_TEST_GLOBAL_KNOWN_REQUIREMENTS "gpu")
 
 # Set default test aborts to 25 minutes, avoids hitting the CI pipeline inactivity timeout usually set to 30 minutes
-ly_set(LY_TEST_DEFAULT_TIMEOUT 1500)
+ly_set(LY_TEST_DEFAULT_TIMEOUT 2500)
 
 # Add the CMake Test targets for each suite if testing is supported
 if(PAL_TRAIT_BUILD_TESTS_SUPPORTED)

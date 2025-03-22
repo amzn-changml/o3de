aa5a7f97f3336570ddf725afde7d78fb994d2280
Mike Chang <changml@amazon.com>
2025-03-22 14:59:04 -0700

    Update Configurations_clang with compiler cache
    Signed-off-by: Mike Chang <changml@amazon.com>

--- cmake/Platform/Common/MSVC/Configurations_clang.cmake
+++ cmake/Platform/Common/MSVC/Configurations_clang.cmake
@@ -7,6 +7,7 @@
 #
 
 include(cmake/Platform/Common/Configurations_common.cmake)
+include(cmake/Platform/Common/MSVC/CompilerCache.cmake)
 
 # Exceptions are disabled by default.  Use this to turn them on just for a specific target.
 # MSVC-Clang uses MSVC compiler option syntax (so /EHsc instead of -fexceptions)
@@ -74,7 +75,6 @@ ly_append_configurations_options(
         /O2             # Maximinize speed, equivalent to /Og /Oi /Ot /Oy /Ob2 /GF /Gy
         /Zc:inline      # Removes unreferenced functions or data that are COMDATs or only have internal linkage
         /Zc:wchar_t     # Use compiler native wchar_t
-        /Zi             # Generate debugging information (no Edit/Continue)
     COMPILATION_RELEASE
         /Ox             # Full optimization
         /Ob2            # Inline any suitable function
@@ -107,3 +107,38 @@ if(LY_BUILD_WITH_ADDRESS_SANITIZER)
 endif()
 include(cmake/Platform/Common/TargetIncludeSystemDirectories_supported.cmake)
 
+# Look for O3DE_ENABLE_COMPILER_CACHE as a CMake flag or environment variable, then sets the appropriate compatible flags for caching
+# More details about the compiler cache can be found in CompilerCache.cmake
+
+if((O3DE_ENABLE_COMPILER_CACHE OR "$ENV{O3DE_ENABLE_COMPILER_CACHE}" STREQUAL "true"))
+    o3de_compiler_cache_activation()
+
+    # Set debug information format for compiler cache compatibility
+    cmake_policy(SET CMP0141 NEW)
+    set(CMAKE_C_COMPILER_LAUNCHER ${CMAKE_BINARY_DIR}/cl.exe)
+    set(CMAKE_CXX_COMPILER_LAUNCHER ${CMAKE_BINARY_DIR}/cl.exe)
+    
+    set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT "Embedded")
+    
+    # Fallback to compiler flags if the debug format doesn't work, which can depend on CMake version
+    ly_append_configurations_options(
+        COMPILATION_PROFILE
+            /Z7             # Use embedded debug info instead of PDB
+        COMPILATION_RELEASE
+            /Z7
+    )
+
+    # Set the tool path and execution settings
+    set(CMAKE_VS_GLOBALS
+        "CLToolExe=cl.exe"
+        "CLToolPath=${CMAKE_BINARY_DIR}"
+        "TrackFileAccess=false"
+        "UseMultiToolTask=true"
+        "DebugInformationFormat=OldStyle"
+    )
+else()
+    ly_append_configurations_options(
+        COMPILATION_PROFILE
+            /Zi             # Generate debugging information (no Edit/Continue)
+    )
+endif()

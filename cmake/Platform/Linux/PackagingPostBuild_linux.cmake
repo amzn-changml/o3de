0783b28495222415cd9c6caf29b7c9141764b348
Mike Chang <62353586+amzn-changml@users.noreply.github.com>
2026-08-01 13:40:33 -0700

    Save installer package as job artifact when S3 upload is not configured

--- cmake/Platform/Linux/PackagingPostBuild_linux.cmake
+++ cmake/Platform/Linux/PackagingPostBuild_linux.cmake
@@ -38,35 +38,39 @@ file(${checksum} ${pack_file} file_checksum)
 file(WRITE ${hash_file} "${file_checksum} ${pack_file}")
 
 if(CPACK_UPLOAD_URL)
-    # use the internal default path if somehow not specified from cpack_configure_downloads
-    if(NOT CPACK_UPLOAD_DIRECTORY)
-        set(CPACK_UPLOAD_DIRECTORY ${CPACK_PACKAGE_DIRECTORY}/CPackUploads)
-    endif()
-
     if(ext STREQUAL "deb")
         # Sign and regenerate checksum
         ly_sign_binaries("${pack_file}" "")
         file(${checksum} ${pack_file} file_checksum)
         file(WRITE ${hash_file} "${file_checksum} ${pack_file}")
     endif()
+endif()
 
-    # Copy the artifacts intended to be uploaded to a remote server into the folder specified
-    # through CPACK_UPLOAD_DIRECTORY. This mimics the same process cpack does natively for
-    # some other frameworks that have built-in online installer support.
-    message(STATUS "Copying packaging artifacts to upload directory...")
-    file(REMOVE_RECURSE ${CPACK_UPLOAD_DIRECTORY})
-    file(GLOB _artifacts 
-        "${CPACK_TOPLEVEL_DIRECTORY}/*.${ext}" 
-        "${CPACK_TOPLEVEL_DIRECTORY}/*.${checkext}"
-        "${LY_ROOT_FOLDER}/scripts/signer/Platform/Linux/*.gpg"
-        "${CPACK_3P_LICENSE_FILE}"
-        "${CPACK_3P_MANIFEST_FILE}"
-    )
-    file(COPY ${_artifacts}
-        DESTINATION ${CPACK_UPLOAD_DIRECTORY}
-    )
-    message(STATUS "Artifacts copied to ${CPACK_UPLOAD_DIRECTORY}")
+# use the internal default path if somehow not specified from cpack_configure_downloads
+if(NOT CPACK_UPLOAD_DIRECTORY)
+    set(CPACK_UPLOAD_DIRECTORY ${CPACK_PACKAGE_DIRECTORY}/CPackUploads)
+endif()
 
+# Copy the artifacts intended to be uploaded to a remote server into the folder specified
+# through CPACK_UPLOAD_DIRECTORY. This mimics the same process cpack does natively for
+# some other frameworks that have built-in online installer support. Populated even
+# without CPACK_UPLOAD_URL so CI can archive the package as a job artifact when no
+# remote upload is configured.
+message(STATUS "Copying packaging artifacts to upload directory...")
+file(REMOVE_RECURSE ${CPACK_UPLOAD_DIRECTORY})
+file(GLOB _artifacts 
+    "${CPACK_TOPLEVEL_DIRECTORY}/*.${ext}" 
+    "${CPACK_TOPLEVEL_DIRECTORY}/*.${checkext}"
+    "${LY_ROOT_FOLDER}/scripts/signer/Platform/Linux/*.gpg"
+    "${CPACK_3P_LICENSE_FILE}"
+    "${CPACK_3P_MANIFEST_FILE}"
+)
+file(COPY ${_artifacts}
+    DESTINATION ${CPACK_UPLOAD_DIRECTORY}
+)
+message(STATUS "Artifacts copied to ${CPACK_UPLOAD_DIRECTORY}")
+
+if(CPACK_UPLOAD_URL)
     ly_upload_to_url(
         ${CPACK_UPLOAD_URL}
         ${CPACK_UPLOAD_DIRECTORY}
